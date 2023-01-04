185065d6e528d20659cf05b5bbcec775783b1ffe
Daniel-Oficina <danicabhern@gmail.com>
2023-01-04 11:27:08 -0600

    Modificación de servicios

---
+++ Application/Services/RegisteredVehiclesServices.cs
@@ -0,0 +1,27 @@
+﻿using Application.Interfaces;
+using AutoMapper;
+using Domain.CustomEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class RegisteredVehiclesServices : IRegisteredVehiclesServices
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        private readonly PaginationOptions _paginationOptions;
+
+        public RegisteredVehiclesServices(IUnitOfWork unitOfWork, IMapper mapper, PaginationOptions paginationOptions)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+            _paginationOptions = paginationOptions;
+        }
+
+        
+    }
+}
