52917586a2d30ffd305af2c70c420ee0caa9c1a1
Daniel-Oficina <danicabhern@gmail.com>
2023-01-05 16:25:16 -0600

    Cambios para merge de vehiculos registrados

--- Application/Services/RegisteredVehiclesServices.cs
+++ Application/Services/RegisteredVehiclesServices.cs
@@ -1,9 +1,14 @@
-﻿using Application.Interfaces;
+﻿using Application.Extensions;
+using Application.Interfaces;
 using AutoMapper;
 using Domain.CustomEntities;
+using Domain.DTOs.Filters;
+using Domain.Entities.Registered_Cars;
+using Domain.Entities.User_Approvals;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -22,6 +27,25 @@ namespace Application.Services
             _paginationOptions = paginationOptions;
         }
 
-        
+        public async Task<PagedList<Vehicle>> GetVehicles(VehicleFilter filter)
+        {
+            filter.PageNumber = filter.PageNumber == 0 ? _paginationOptions.DefaultPageNumber : filter.PageNumber;
+            filter.PageSize = filter.PageSize == 0 ? _paginationOptions.DefaultPageSize : filter.PageSize;
+
+            string properties = "";
+            IEnumerable<Vehicle> vehicles = null;
+            Expression<Func<Vehicle, bool>> Query = null;
+
+            if (filter.CreatedAfter.HasValue)
+            {
+                if (Query != null)
+                {
+                    Query = Query.And(p => p.CreatedDate >= filter.CreatedAfter.Value);
+                }
+                else { Query = p => p.CreatedDate >= filter.CreatedAfter.Value; }
+            }
+
+            return null;
+        }
     }
 }
