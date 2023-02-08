b7de2902ccdbde15a9e251a60665731d9ede4d8a
Daniel-Oficina <danicabhern@gmail.com>
2023-02-08 13:38:56 -0600

    Modificicaciones a Vehiculos, Added KM

---
+++ Infrastructure/Persistence/Data/20230208191640_VehicleKMFix.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Data
+{
+    /// <inheritdoc />
+    public partial class VehicleKMFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CurrentKM",
+                table: "Vehicles",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CurrentKM",
+                table: "Vehicles");
+        }
+    }
+}
